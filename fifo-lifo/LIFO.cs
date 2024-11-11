f18adaa3d722b07485b389ce3a70372c1d70a533
Sushkoo <szilagyidominik005@gmail.com>
2024-11-11 10:53:32 +0100

    Add project files.

---
+++ fifo-lifo/LIFO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifo_lifo
+{
+    public class LIFO
+    {
+        double[] array;
+        private int stackPointer;
+
+        public LIFO()
+        {
+            stackPointer = 0;
+            array = new double[50];
+        }
+
+        public LIFO(int size)
+        {
+            stackPointer = 0;
+            array = new double[size];
+        }
+
+        public void Pop(double element) 
+        {
+            array[stackPointer] = element;
+            if (stackPointer < array.Length)
+            {
+                 
+            }
+            else
+            {
+                throw new Exception("Üres");
+            }
+        }
+
+        public void Push(double element)
+        {
+            if (stackPointer < array.Length)
+            {
+                array[stackPointer++] = element;
+            }
+            else
+            {
+                throw new StackOverflowException("SAJÁT : Stack overflow!");
+            }
+            
+        }
+
+        public void Clear(int size)
+        {
+            stackPointer = 0;
+        }
+
+        public int Length()
+        {
+            int seged = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                seged += i;
+            }
+            return seged;
+        }
+
+        public void GetSize()
+        {
+            array.Count();
+        }
+
+
+    }
+}
---
+++ fifo-lifo/Program.cs
@@ -0,0 +1,17 @@
+﻿using fifo_lifo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        LIFO verem = new LIFO(3);
+        verem.Push(123.56);
+        verem.Push(-12.56);
+        verem.Push(3.6);
+        verem.Pop(0);
+
+        Console.WriteLine(verem.Length());
+    }
+
+
+}
